95c7f8110f155d0d5fc30e122de63f037451b029
Ricardo Leon <rickyroses@hotmail.com>
2025-05-06 23:02:22 -0400

    fetch: Se agrego API

--- src/config/constants.js
+++ src/config/constants.js
@@ -2,7 +2,9 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-export const PORT = process.env.PORT_APP
+export const port = process.env.PORT_APP
+export const uri = process.env.URI_APP
+
 export const mongoDomain = process.env.MONGO_DOMAIN
 export const mongoUser = process.env.MONGO_USER
 export const mongoPWD = process.env.MONGO_PWD
